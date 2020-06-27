414d66e6a62cd4e56eda2442a12581632df3024c
jakub <jacob_the_liar@tlen.pl>
2020-06-27 08:16:18 +0200

    initial commit

---
+++ src/main/java/pl/jacob_the_liar/module/web_files/WebFilesApplication.java
@@ -0,0 +1,13 @@
+package pl.jacob_the_liar.module.web_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jacob_the_liar/module/web_files/WebFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacob_the_liar.module.web_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
